dd003f6b2747ad5b085802aa10e9c6129ec9cb6f
tharlys10 <tharlys2015c@gmail.com>
2021-10-19 11:48:54 -0300

    refactor: :see_no_evil: Update file gitignore

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 export APP_PORT=3000
 export JWT_SECRET=BOOK_API
-export DB_HOST=
-export DB_PORT=
-export DB_NAME=
-export DB_USER=
-export DB_PASS=
+export DB_HOST=localhost
+export DB_PORT=5432
+export DB_NAME=books
+export DB_USER=developer
+export DB_PASS=es12t13o2
 
 rm -rv ./*.bin; go build -v -o application.bin
 ./application.bin
